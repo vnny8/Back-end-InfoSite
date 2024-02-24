3943ace65e7178574849602ab6cd9533615528de
Juan Cassemiro <juancssemiro@gmail.com>
2024-02-23 21:02:20 -0400

    Correção cors

--- src/main/java/com/ValidaAPI/Projeto/infra/security/SecurityConfigurations.java
+++ src/main/java/com/ValidaAPI/Projeto/infra/security/SecurityConfigurations.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -32,6 +33,7 @@ public class SecurityConfigurations {
                             req.requestMatchers("/api/projetos").permitAll();
                             req.anyRequest().authenticated();
                         })
+                        .cors(AbstractHttpConfigurer::disable)
                         .addFilterBefore(filterSecurity, UsernamePasswordAuthenticationFilter.class)
                         .build();
     }
